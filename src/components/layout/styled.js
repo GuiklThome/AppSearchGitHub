d72842b6e87b0d2cac5702d2a46c29b902c38a4f
GuiklThome <GuiThome_Santos@outlook.com>
2021-11-22 21:53:36 -0300

    SeachGitHub

---
+++ src/components/layout/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const WrapperLayout = styled.section`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  background-color: #a9a9a9;
+  margin: 0px;
+`;
